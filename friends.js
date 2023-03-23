8124b9645312f21bf77c395fccce6756e091ab67
EyUp Student <eyupstudent@EyUps-MacBook-Air.local>
2023-03-23 16:43:28 +0000

    not yet

--- friends.js
+++ friends.js
@@ -6,7 +6,7 @@ const habbit = document.getElementById("habbit");
 const output= document.getElementById('output');
 const addHabbitButton = document.getElementById("addHabit");
 const addFriendsButton = document.getElementById("addFriends");
-let collectedHabbit = "";
+let collectedHabbit = ""; const allHabits=[];
 
 
 const form = document.querySelector('form');
@@ -14,6 +14,7 @@ const form = document.querySelector('form');
 //Add Habbit
 function habbitCollector () {
     collectedHabbit = habbit.value;
+    allHabits.push(collectedHabbit);
     appendHabbit();
 }
  function appendHabbit() {
@@ -24,7 +25,7 @@ function habbitCollector () {
     button.textContent = "Delete Habit";
     button.onclick = () => {button.parentElement.remove()};
     habbitBox.appendChild(button);
-    habbitContainer.appendChild(habbitBox)
+    habbitContainer.appendChild(habbitBox);
  }
 
 const makePElement = (key, value) => {
@@ -36,11 +37,19 @@ const makePElement = (key, value) => {
 form.onsubmit = (event) => {
     event.preventDefault();
     // const formValues=[];
-    for (let i = 0; i < form.length - 1; i++) {
-        // formValues[form[i].name] = form[i].value
-        makePElement(form[i].name, form[i].value)
+    for (let i = 0; i < form.length - 2; i++) {
+
+        if (form[i].id === 'habbit') {
+            let array = document.createElement('p');
+            array.textContent = allHabits;
+            output.appendChild(array);
+        } else {
+            // formValues[form[i].name] = form[i].value
+            makePElement(form[i].name, form[i].value)
+        }
         // console.log(form[i].firstName, form[i].value);
     }
+    console.log(allHabits);
 }
 
 addHabbitButton.onclick = habbitCollector
